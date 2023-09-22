bc254c88695f6beafe14cbc0b067eb9ce3985e63
solidlucho <solidlucho@gmail.com>
2023-09-22 18:19:59 -0300

    pop up

--- SuperNintendoDB/Features/HomeViewController.swift
+++ SuperNintendoDB/Features/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class HomeViewController: UIViewController {
 
     
+    @IBOutlet weak var aboutBtn: UIButton!
     
     @IBOutlet weak var toGameListButton: UIButton!
     override func viewDidLoad() {
@@ -19,5 +20,18 @@ class HomeViewController: UIViewController {
     }
     
  
+    @IBAction func AboutAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "About", message: "Create by SolidLucho", preferredStyle: .alert)
 
+              // Agrega un botón "Aceptar" al popup
+              let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
+                  // Aquí puedes realizar alguna acción cuando se presiona "Aceptar"
+                  print("Botón Aceptar presionado")
+              }
+              alertController.addAction(okAction)
+
+              // Presenta el popup en la pantalla actual
+              present(alertController, animated: true, completion: nil)
+    }
+    
 }
